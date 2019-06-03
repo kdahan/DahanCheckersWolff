5deb16f7f6251e5ab7b8d8e8f86280c65bc01818
kdahan <kerenasaurus@gmail.com>
2019-06-03 11:35:56 -0400

    main

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JPanel {
+
+    public Main(int w, int h){
+        setSize(w, h);
+
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+    }
+
+    public static void main(String[] args) {
+        //The JFrame class represents the window that holds the graphics
+        JFrame window = new JFrame("Images!");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, 600, 800 + 22); //(x, y, w, h) 22 due to title bar.
+
+        Main panel = new Main(600, 800);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        window.add(panel);
+        window.setVisible(true);
+//        window.setResizable(false);
+    }
+
+}
